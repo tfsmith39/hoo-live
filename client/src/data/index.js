7b50b1fd7c2db2f03b2ebd7c81e6729080143f6b
tfsmith39 <tfsmith53@gmail.com>
2020-05-21 13:42:08 -0500

    Styling

--- client/src/data/index.js
+++ client/src/data/index.js
@@ -1,20 +1,9 @@
-import AmazonIcon from "../res/amazon.png";
 import FacebookIcon from "../res/facebook.png";
-import GithubIcon from "../res/github.png";
 import GoogleIcon from "../res/google.png";
-import InstagramIcon from "../res/instagram.png";
-import SpotifyIcon from "../res/spotify.png";
 import TwitchIcon from "../res/twitch.png";
 import MixerIcon from "../res/mixer.png";
 
 export const data = [{
-    img: AmazonIcon,
-    name: "amazon",
-    href: "auth/amazon",
-    alt: "amazon-icon",
-    color: "#F9AE31",
-    txt: "Login with Amazon"
-},  {
     img: FacebookIcon,
     name: "facebook",
     href: "auth/facebook",
@@ -22,40 +11,12 @@ export const data = [{
     color: "#3B5899",
     txt: "Login with Facebook"
 }, {
-    img: GithubIcon,
-    name: "github",
-    href: "auth/github",
-    alt: "github-icon",
-    color: "#333333",
-    txt: "Login with Github"
-}, {
     img: GoogleIcon,
     name: "google",
     href: "auth/google",
     alt: "google-icon",
     color: "#CB4024",
     txt: "Login with Google"
-}, {
-    img: InstagramIcon,
-    name: "instagram",
-    href: "auth/instagram",
-    alt: "instagram-icon",
-    colors: {
-        leftBot: "#fec564",
-        leftTop: "#5258cf",
-        rightTop: "#893dc2",
-        rightBot: "#d9317a",
-        baseCoat: "linear-gradient(#6559ca, #bc318f 30%, #e33f5f 50%, #f77638 70%, #fec66d 100%)",
-    },
-    color: "#d9317a",
-    txt: "Login with Instagram"
-}, {
-    img: SpotifyIcon,
-    name: "spotify",
-    href: "auth/spotify",
-    alt: "spotify-icon",
-    color: "#1EB954",
-    txt: "Login with Spotify"
 }, {
     img: TwitchIcon,
     name: "twitch.js",
@@ -68,5 +29,6 @@ export const data = [{
     name: "mixer.js",
     href: "auth/mixer",
     alt: "mixer-icon",
+    color: "#2B3244",
     txt: "Login with Mixer"
 }];
